a58a2557831c55f12f0fdca0579c1054e85a3a3e
Gregory Travis <greg.m.travis@gmail.com>
2022-03-27 12:59:50 -0400

    recipes

--- src/Random.hs
+++ src/Random.hs
@@ -1,15 +1,19 @@
 module Random
-( randomShape ) where
+( randomShape
+, recipe
+, crecipe
+, thang2 ) where
 
 import System.Random
 
 import BinOp
+import Color
 import E
 import Funs
 import Grid
 import Prim
 import Transform
-import Util
+import Util hiding (time)
 
 randomPrim :: IO Shape
 randomPrim = randFromList allPrims
@@ -84,3 +88,68 @@ randomShapes = [
   , (randomUnOp <*>) randomPrim
   , (randomBinOp <*>) randomPrim <*> randomPrim
   ]
+
+recipe :: IO Shape
+recipe = randIO recipes
+  where recipes = [
+            thang
+          , return filaoa
+          , return anotherGreatOne
+          -- , return hmm
+          ]
+
+crecipe :: IO E
+crecipe = do
+  col <- randomColor
+  r <- recipe
+  let camera = scale 1.0
+      color = smooth col nothing $ evalShape (camera r)
+  return color
+
+thang :: IO Shape
+thang = do
+  rs0 <- randomShape
+  rs1 <- randomShape
+  let rs0' = rotation (osc 0.5) (pfGrid 2 2 rs0)
+  let rs1' = rotation (osc (-0.35)) (pfGrid 1.5 1.5 rs1)
+  let p = interp (osc 0.2) rs0' rs1'
+  return $ scale 0.1 p
+
+thang2 :: IO E
+thang2 = do
+  a <- translation (V2 (time * 0.8) 0.0) <$> recipe 
+  b <- rotation (time * 0.2) <$> recipe
+  c <- recipe
+  cola <- randomColor
+  colb <- randomColor
+  colc <- randomTransparentColor
+  let camera = scale 0.1
+      acolor = smooth cola nothing $ evalShape $ camera a
+      bcolor = smooth colb nothing $ evalShape $ camera b
+      ccolor = smooth colc nothing $ evalShape $ camera c
+      color = alphaBlends [acolor, bcolor, ccolor]
+  return color
+
+-- FAVORITE don't lose this!!
+-- fall in love all over again
+filaoa :: Shape
+filaoa = scale 0.1 $ smoothUnion (scale (time / 10.0) filaoa') (rotation (time / 10.0) filaoa')
+  where filaoa' = pfGrid 2.25 2.25 circle
+
+anotherGreatOne :: Shape
+anotherGreatOne =
+  let ss = rotation (time * 0.4) $ pfGrid 2.5 2.5 square
+      cs = rotation (time * (-0.3)) $ pfGrid 2.5 2.5 circle
+      gridz = interp (osc 0.5) ss cs
+      morph = interp (osc 2) gridz filaoa
+   in morph
+
+-- bloop experiment
+hmm :: Shape
+hmm = do
+  let cir = (translation (V2 (- (time * 0.08)) 0.0)) $ (scale 0.15) circle
+      smaller = (translation (V2 (- (time * 0.08)) 0.0)) $ (scale 0.03) circle
+      both = smoothUnion square cir
+      p' = difference both cir
+      p3 = union p' smaller
+   in scale 0.1 p3
