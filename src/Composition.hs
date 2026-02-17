572c4ac8c59c91d5d0b0ddaa7489789c811345d3
Gregory Travis <greg.m.travis@gmail.com>
2026-02-17 16:10:05 -0500

    composeCircular builds

--- src/Composition.hs
+++ src/Composition.hs
@@ -3,6 +3,7 @@ module Composition
 
 import E
 import Color
+import Lib
 import Prim
 import Util hiding (time)
 
@@ -10,13 +11,12 @@ import Util hiding (time)
 -- composeCircular :: (Transform -> E Float) -> (Transform -> E Float) -> (Transform -> E Float)
 composeCircular :: Shape -> Shape -> Shape
 composeCircular s0 s1 tr =
-  let d0 = s0 tr + 1.0 // -1 -> 0
+  let d0 = s0 tr + 1.0 -- -1 -> 0
       d0 :: E Float
-      (gx, gy) = (sdFdx d0, sdFdy d0)
-      (gx, gy) :: (E Float, E Float)
+      (gx, gy) = (sdFdx d0, sdFdy d0) :: (E Float, E Float)
       theta = satan gy gx
-      ux = cos theta
-      uy = sin theta
+      ux = scos theta
+      uy = ssin theta
       x = d0 * ux
       y = d0 * uy
       ctr = Transform (V2 x y) time
